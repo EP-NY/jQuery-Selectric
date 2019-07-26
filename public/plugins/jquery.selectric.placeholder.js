0720763fc6e9ed913aedf0816fbb22a152a48917
Joseph Bergdoll <joseph@ep-ny.com>
2019-07-26 11:20:46 -0400

    Update to 1.15.0

--- public/plugins/jquery.selectric.placeholder.js
+++ public/plugins/jquery.selectric.placeholder.js
@@ -1,4 +1,4 @@
-/*! Selectric Placeholder ϟ v0.2.0 (2017-08-22) - git.io/tjl9sQ - Copyright (c) 2017 Leonardo Santos - MIT License */
+/*! Selectric Placeholder ϟ v0.2.0 (2019-07-26) - git.io/tjl9sQ - Copyright (c) 2019 Leonardo Santos - MIT License */
 (function(factory) {
   /* global define */
   if ( typeof define === 'function' && define.amd ) {
